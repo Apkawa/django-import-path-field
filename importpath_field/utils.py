16e45621b519f13147ed9e980ffaefd1c79d0940
apkawa <apkawa@gmail.com>
2020-01-08 15:56:51 +0300

    Add some tests; fix py35

--- importpath_field/utils.py
+++ importpath_field/utils.py
@@ -4,7 +4,7 @@ import types
 
 import six
 
-from .compat import import_string as _import_string
+from importpath_field import compat
 
 
 def is_class_method(klass, attr, value=None):
@@ -32,12 +32,21 @@ def is_class_method(klass, attr, value=None):
 
 
 def import_string(path):
+    """
+    >>> import_string('django.db.models.Model')
+    <class 'django.db.models.base.Model'>
+    >>> import_string('django.db.models.Model.from_db')
+    <bound method Model.from_db of <class 'django.db.models.base.Model'>>
+    >>> import_string('django.db.models.FooBar')
+    Traceback (most recent call last):
+    ImportError: Module "django.db.models" does not define a "FooBar" attribute/class
+    """
     try:
-        return _import_string(path)
-    except ModuleNotFoundError:
+        return compat.import_string(path)
+    except compat.ModuleNotFoundError:
         # Maybe is class attribute
         class_path, attr_name = path.rsplit('.', 1)
-        _cls = _import_string(class_path)
+        _cls = compat.import_string(class_path)
         try:
             return getattr(_cls, attr_name)
         except AttributeError:
@@ -47,6 +56,16 @@ def import_string(path):
 
 
 def get_path(obj):
+    """
+    >>> from django.db import models
+    >>> get_path(models)
+    'django.db.models'
+    >>> get_path(models.Model)
+    'django.db.models.base.Model'
+    >>> get_path(models.Model.from_db)
+    'django.db.models.base.Model.from_db'
+
+    """
     if obj is None:
         return None
     if isinstance(obj, types.ModuleType):
